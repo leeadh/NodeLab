1210a1670c65344e884a9f9fd765c2f045331447
Adrian <leexhadrian@gmail.com>
2017-11-22 00:34:28 +0800

    first commit

---
+++ hello-server.js
@@ -0,0 +1,19 @@
+// hello-server.js
+
+const http = require('http') 
+const port = 4200 // define whatever port you like
+
+const requestHandler = (request, response) => { 
+ console.log(request.url)
+ response.end('Hello World')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => { 
+ if (err) {
+ return console.log('something happened', err)
+ }
+
+ console.log(`server is listening on ${port}`)
+})
