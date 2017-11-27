8bb1c366c550cfff045dd17e9c0e60a2c83ecb00
Adrian <leexhadrian@gmail.com>
2017-11-27 10:47:43 +0800

    changed mongoose

--- mongoose_connector.js
+++ mongoose_connector.js
@@ -2,39 +2,20 @@
 // === ESTABLISH a mongo DB connection and query all objects from the db === //
 
 var mongoose = require('mongoose');
-var express = require ('express');
-var bodyParser = require('body-parser');
-var app = express();
-
-//configure app to use bodyParser()
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var port = process.env.PORT||3000;
-
-var router = express.Router();
-
-router.get('/',function(req,res){
-	res.json({message: 'hello'});
-});
-
-
-app.use('/api',router)
-app.listen(port);
 
 mongoose.connect('mongodb://demo_user1:password@localhost:27017/simple_demo');
 
 var db = mongoose.connection;
+
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // opening the connection and query all objects
 db.on('open', function () {
-  	db.db.collection('employee',function (err, collection) {
-  		collection.find().toArray(function(err,items){
-  			if (err) throw err;
-  			console.log(items);
-
-  		});
-
-	});
+	var cursor = db.getCollection('employee').find({},{});
+	while(cursor.hasNext()){
+		console.log(cursor.next());
+	};
 });
+
+// === Rendering in the front end === /
