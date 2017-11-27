d31b7c48d5017a42dddc708a9a494bf5ee2a08af
Adrian <leexhadrian@gmail.com>
2017-11-27 11:33:54 +0800

    changed mongoose

--- mongoose_connector.js
+++ mongoose_connector.js
@@ -1,43 +1,23 @@
+var express = require('express');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://demo_user1:password@localhost:27017/simple_demo';
+var str = "";
 
-// === ESTABLISH a mongo DB connection and query all objects from the db === //
-const http = require('http') 
-const port = 8000 // define whatever port you like
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://demo_user1:password@localhost:27017/simple_demo');
-var db = mongoose.connection;
-var json = [];
-
-
-const requestHandler = (request, response) => { 
-	console.log(request.url)
-	response.end('test')
-}
-
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-db.on('open', function () {
-  	db.db.collection('employee',function (err, collection) {
-  		collection.find().toArray(function(err,items){
-  			if (err) throw err;
-  			items.forEach(function(item){
-  				json.push(item);
-  				
-  			});
-  			console.log(json);
-  		});
-	});
+app.route('/Employeeid').get(function(req, res) {
+   MongoClient.connect(url, function(err, db) {
+       var collection = db.collection('employee');
+       var cursor = collection.find({});
+       str = "";
+       cursor.forEach(function(item) {
+           if (item != null) {
+                   str = str + "    Employee id  " + item._id + "</br>";
+           }
+       }, function(err) {
+           res.send(str);
+           db.close();
+          }
+       );
+   });
 });
-
-
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => { 
-	if (err) {
-		return console.log('something happened', err)
-	}
-	console.log(`server is listening on ${port}`)
-})
-
-
-// === Rendering in the front end === /
+var server = app.listen(8000, function() {});
