745156c43a8597d82c80bbf73663784162f9166a
Adrian <leexhadrian@gmail.com>
2017-11-27 10:51:35 +0800

    changed mongoose

--- mongoose_connector.js
+++ mongoose_connector.js
@@ -7,15 +7,23 @@ mongoose.connect('mongodb://demo_user1:password@localhost:27017/simple_demo');
 
 var db = mongoose.connection;
 
-
+var json = [];
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // opening the connection and query all objects
 db.on('open', function () {
-	var cursor = db.db.getCollection('employee').find({},{});
-	while(cursor.hasNext()){
-		console.log(cursor.next());
-	};
+  	db.db.collection('employee',function (err, collection) {
+  		collection.find().toArray(function(err,items){
+  			if (err) throw err;
+  			items.forEach(function(item){
+  				json.push(item);
+  			});
+
+  		});
+
+	});
 });
+
+console.log(json);
 
 // === Rendering in the front end === /
