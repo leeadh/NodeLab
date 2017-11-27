d5639b3b1b490e75235606e905ae72c331d3afda
Adrian <leexhadrian@gmail.com>
2017-11-27 11:43:58 +0800

    changed mongoose

--- mongoose_connector.js
+++ mongoose_connector.js
@@ -1,12 +1,25 @@
-'use strict';
-
-var express = require('express'),
-	routes = require('./app/routes/index.js');
-
+var express = require('express');
 var app = express();
 
-routes(app);
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://demo_user1:password@localhost:27017/simple_demo';
+var str = "";
 
-app.listen(3000, function () {
-	console.log('Listening on port 3000...');
+app.get('/',function(req, res) {
+   MongoClient.connect(url, function(err, db) {
+       var collection = db.collection('employee');
+       var cursor = collection.find({});
+       str = "";
+       cursor.forEach(function(item) {
+           if (item != null) {
+                   str = str + "    Employee id  " + item._id + "</br>";
+           }
+       }, function(err) {
+           res.send(str);
+           db.close();
+          }
+       );
+   });
 });
+
+var server = app.listen(8000, function() {});
