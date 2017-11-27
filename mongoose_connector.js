7b09475b3a025c8e65398e9a8d139225bc6485a9
Adrian <leexhadrian@gmail.com>
2017-11-27 10:05:32 +0800

    changed mongoose

--- mongoose_connector.js
+++ mongoose_connector.js
@@ -1,11 +1,19 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://demo_user1:password@localhost:27017/simple_demo');
+
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
+
 var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 db.on('open', function () {
-  	console.log("connection ok");
-  	db.db.listCollections().toArray(function (err, names) {
-  		console.log(names); // [{ name: 'dbname.myCollection' }]
-    	module.exports.Collection = names;
+  	db.db.collection('employee',function (err, collection) {
+  		collection.find().toArray(function(err,items){
+  			console.log(items);
+
+  		});
+
 	});
 });
